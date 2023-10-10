c959852ebe204ecc8049c227de2881309ac012b5
Ayush-Manware <ayushmanware19@gmail.com>
2023-10-10 14:02:06 +0530

    Static Data

--- src/navbar/Student.jsx
+++ src/navbar/Student.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Student = () => {
   return (
@@ -15,6 +16,48 @@ const Student = () => {
             <th>Course</th>
             <th>Batch</th>
             <th>Change</th>
+          </tr>
+          <tr>
+            <td>Ayush</td>
+            <td>21</td>
+            <td>MERN</td>
+            <td>EA25</td>
+            <td><Link>Edit</Link></td>
+          </tr>
+          <tr>
+            <td>Piyush</td>
+            <td>21</td>
+            <td>MEAN</td>
+            <td>EA25</td>
+            <td><Link>Edit</Link></td>
+          </tr>
+          <tr>
+            <td>Suraj</td>
+            <td>23</td>
+            <td>MEAN</td>
+            <td>EA23</td>
+            <td><Link>Edit</Link></td>
+          </tr>
+          <tr>
+            <td>Prateek</td>
+            <td>24</td>
+            <td>MERN</td>
+            <td>EA25</td>
+            <td><Link>Edit</Link></td>
+          </tr>
+          <tr>
+            <td>Aman</td>
+            <td>19</td>
+            <td>MERN</td>
+            <td>EA25</td>
+            <td><Link>Edit</Link></td>
+          </tr>
+          <tr>
+            <td>Krishna</td>
+            <td>17</td>
+            <td>MERN</td>
+            <td>EA25</td>
+            <td><Link>Edit</Link></td>
           </tr>
         </table>
       </div>
